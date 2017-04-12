fc52c7af8d1c695558b28bc44cd4cefaf84e9578
L-Elyse <lguimont@student.rcc.edu>
2017-04-12 00:28:51 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Project/Mastermind_v1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on April 11, 2017, 10:23 PM
+ * Purpose: Mastermind AI
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the Random Number Seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Declare Variables
+    
+    
+    //Exit Stage Right!
+    return 0;
+}
+
