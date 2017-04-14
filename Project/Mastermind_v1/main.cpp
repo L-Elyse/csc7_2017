cce79f5aa738e3e18a554d6a32a87136dcfeb910
L-Elyse <lguimont@student.rcc.edu>
2017-04-13 23:15:42 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Project/Mastermind_v1/main.cpp
+++ Project/Mastermind_v1/main.cpp
@@ -1,7 +1,7 @@
 /* 
  * File:   main.cpp
  * Author: Laurie Guimont
- * Created on April 11, 2017, 10:23 PM
+ * Created on April 13, 2017, 11:23 PM
  * Purpose: Mastermind AI
  */
 
@@ -11,15 +11,40 @@
 #include <ctime>
 using namespace std;
 
+//Function Prototypes
+int *fillAry(int *,const int);
+
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Set the Random Number Seed
     srand(static_cast<unsigned int>(time(0)));
     
     //Declare Variables
+    const int SIZE=4;
+    int array[SIZE];
+    int brray[SIZE];
+    int black=0,white=0,count=0;
+    int guess;
     
+    //Intro
+    cout<<"Let's play a game of Mastermind. The object of the game is to ";
+    cout<<"guess the 4-digit code that is comprised of the numbers 1-6."<<endl;
+    fillAry(array,SIZE);
+    cout<<"The computer just picked the code. Time for you to guess!"<<endl;
+    cout<<"Guess #"<<count+1<<": ";
+    cin>>guess;    
     
     //Exit Stage Right!
     return 0;
+}
+
+int *fillAry(int *a,const int n){
+    //Declare Minimum and Maximum Values to Choose
+    const int MIN=1, MAX=6;
+    
+    for(int i=0;i<n;i++)
+        *(a+i)=(rand()%(MAX-MIN+1))+MIN;
+    
+    return a;
 }
 
