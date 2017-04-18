42cb64c76a0d24ce6f376a6341e75395de7b665c
L-Elyse <lguimont@student.rcc.edu>
2017-04-18 01:18:04 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Project/Mastermind_v1/main.cpp
+++ Project/Mastermind_v1/main.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 //Function Prototypes
 int *fillAry(int *,const int);
+int *guesAry(int *,int,const int);
+int compare(int *,int *,const int);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -23,7 +25,7 @@ int main(int argc, char** argv) {
     const int SIZE=4;
     int array[SIZE];         //Array of the Code
     int brray[SIZE];         //Array of the Guess
-    int black=0,white=0,count=0;
+    int count=0;
     int guess;
     
     //Intro
@@ -34,17 +36,43 @@ int main(int argc, char** argv) {
     cout<<"Guess #"<<count+1<<": ";
     cin>>guess;    
     
+    //Put Guess in an Array
+    guesAry(brray,guess,SIZE);
+    compare(array,brray,SIZE);
+    
     //Exit Stage Right!
     return 0;
+}
+
+int compare(int *a,int *b,const int n){
+    int black=0,white=0;
+    
+    for(int i=0;i<n;i++){
+        if(a[i]==b[i])
+            black++;
+    }
+    cout<<"Black: "<<black<<endl;
+    cout<<"White: "<<white<<endl;
+}
+
+int *guesAry(int *b,int a,const int n){    
+    //Fill Guess Array
+    *(b+0)=a/1000;
+    *(b+1)=(a/100)-(*(b+0)*10);
+    *(b+2)=(a/10)-(*(b+0)*100)-(*(b+1)*10);
+    *(b+3)=a%10;
+    
+    return b;
 }
 
 int *fillAry(int *a,const int n){
     //Declare Minimum and Maximum Values to Choose
     const int MIN=1, MAX=6;
     
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++){
         *(a+i)=(rand()%(MAX-MIN+1))+MIN;
+        cout<<*(a+i)<<" ";
+    }
     
     return a;
 }
-
